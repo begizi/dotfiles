7cc46983afb4dc1787f85c692354f08f944bf773
Brian Egizi <me@brianegizi.com>
2016-06-01 13:26:48 -0400

    [zsh] Log all future shell commands
    May save my ass one day

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -2,3 +2,6 @@
 function docker-up () {
   eval "$(docker-machine env default)"
 }
+
+# Log commands profile log folder for future use
+precmd() { eval 'echo "$(date "+%Y-%m-%d.%H:%M:%S") $(pwd) $(history | tail -n1 | sed "s/^[ ]*[0-9]\+[ ]*//" )" >> ~/.logs/bash-history-$(date "+%Y-%m-%d").log'}
